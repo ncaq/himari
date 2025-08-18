f113d6d4bbc1ae0d641ab7d687a9f27ef1f830e9
ncaq <ncaq@ncaq.net>
2025-08-18 14:58:27 +0900

    build: flake.nixによるNix flakeセットアップを追加

---
+++ flake.nix
@@ -0,0 +1,157 @@
+{
+  inputs = {
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+      };
+    };
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      haskellNix,
+    }:
+    # 現状はLinuxのみを想定。
+    let
+      ghc-version = "ghc9102";
+      overlays = [
+        haskellNix.overlay
+        (
+          final: prev:
+          let
+            # haskell.nixのtoolsで参照されるhaskell-language-server。
+            tool-haskell-language-server =
+              final.haskell-nix.tool ghc-version "haskell-language-server"
+                "latest";
+          in
+          {
+            # nixpkgsで普通にインストールされるfourmoluはhaskell-language-serverのものと違うので上書きして合わせる。
+            inherit (tool-haskell-language-server.project.hsPkgs.fourmolu.components.exes) fourmolu;
+          }
+        )
+        (final: prev: {
+          project = final.haskell-nix.cabalProject' {
+            src = ./.;
+            compiler-nix-name = ghc-version;
+            modules = [
+              # `nix flake check`レベルではcabalの警告をエラーとして扱います。
+              # ライブラリの問題ない範囲の不一致とか考えるとcabalの警告はエラーにしないべきですが、
+              # CIでは通したくないので警告も含めてエラーにします。
+              # CI専用に環境を分離するのも手ですが、
+              # 元々`nix flake check`では最適化を無効にするのが面倒なので時間がかかるため、
+              # あまり反復的に実行しません。
+              # 反復的に実行してテストの結果とかを確認するのには普通は`cabal test`のような言語固有のコマンドを使います。
+              # `cabal test`の方が実行するテストをフィルタリングとかも簡単に出来ますし。
+              # そのことを考えると本番を考えてエラーにしても構わないでしょう。
+              # flake参照された時にnixpkgsをfollowすると問題ない警告をエラーにしてしまうかもしれないのが懸念点ですが、
+              # 少なくとも現在は考慮する必要はないでしょう。
+              # 注意点として、srcやtestはビルドされますが、executableであるappはビルドされません。
+              # 「appはエントリーポイントとしてのみ使う」習慣を守っていれば問題にはならないです。
+              (
+                { lib, config, ... }:
+                {
+                  # パッケージたちをハードコーディングすると変更忘れが発生するので`config.package-keys`で取得。
+                  options.packages = lib.genAttrs config.package-keys (
+                    _name:
+                    lib.mkOption {
+                      type = lib.types.submodule (
+                        { config, lib, ... }:
+                        # `cabal.project`に`source-repository-package`などで書かれていたりする、
+                        # 外部パッケージは変更したくないので、
+                        # `isProject`でフィルタリングしています。
+                        lib.mkIf config.package.isProject {
+                          # この書き方で上書きではなく追加として扱われる。
+                          ghcOptions = [ "-Werror" ];
+                        }
+                      );
+                    }
+                  );
+                }
+              )
+            ];
+            shell = {
+              tools = {
+                cabal = "latest";
+                cabal-gild = "latest"; # treefmtで管理されているがvscodeのHaskell拡張向けに使えるようにしておく
+                haskell-language-server = "latest";
+                implicit-hie = "latest";
+              };
+              # ランタイム依存。
+              buildInputs = with prev; [
+                fourmolu
+                nil
+                nixfmt-rfc-style
+                yamllint
+
+                (writeScriptBin "haskell-language-server-wrapper" ''
+                  #!${stdenv.shell}
+                  exec haskell-language-server "$@"
+                '')
+              ];
+            };
+          };
+        })
+      ];
+    in
+    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        flake = pkgs.project.flake { };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs (_: {
+          # actionlintはセルフホストランナーの設定ファイルを正常に読み込まなかった。
+          # yamlfmtはprettierと競合する。
+          projectRootFile = "flake.nix";
+          programs = {
+            cabal-gild.enable = true;
+            deadnix.enable = true;
+            hlint.enable = true;
+            nixfmt.enable = true;
+            prettier.enable = true;
+            shellcheck.enable = true;
+            shfmt.enable = true;
+            statix.enable = true;
+
+            fourmolu = {
+              enable = true;
+              package = pkgs.fourmolu;
+            };
+          };
+        });
+      in
+      flake
+      // {
+        checks =
+          flake.checks
+          // flake.packages # テストがないパッケージもビルドしてエラーを検出する。
+          // {
+            formatting = treefmtEval.config.build.check self;
+          };
+        formatter = treefmtEval.config.build.wrapper;
+      }
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+}
