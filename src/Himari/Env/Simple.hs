cce83a4452455029ac880fe0bae937335236fd7f
ncaq <ncaq@ncaq.net>
2025-11-19 13:17:26 +0900

    refactor(logger): ログ出力型とフィールド名をLogActionに統一しAPIを整理
    - loggerフィールドをlogActionにリネームし、型をLogActionに統一
    - HasLogger型クラスをHasLogAction型クラスに変更
    - LogAction型シノニムを追加し、ログ出力関数の型を明確化
    - SimpleEnvのrunSimpleEnv, runSimpleEnvWith関数を追加し、デフォルト/カスタム出力での実行をサポート

--- src/Himari/Env/Simple.hs
+++ src/Himari/Env/Simple.hs
@@ -1,19 +1,35 @@
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
 -- | 基本的な環境を提供するモジュール。
 module Himari.Env.Simple
   ( SimpleEnv
+  , runSimpleEnv
+  , runSimpleEnvWith
   ) where
 
+import Himari.Env
 import Himari.Logger
 import Himari.Prelude
 
 -- | シンプルにロガーなどを持って解決するためのコンテキスト。
 newtype SimpleEnv = SimpleEnv
-  { logger :: Loc -> LogSource -> LogLevel -> LogStr -> IO ()
+  { logAction :: LogAction
   -- ^ ログフレームワークに渡すための関数。
+  -- デフォルトではmonad-loggerによって標準エラー出力にログを出力する。
   }
   deriving (Generic)
 
 makeFieldsId ''SimpleEnv
+
+-- | `SimpleEnv`をデフォルト設定で実行する。
+runSimpleEnv :: (MonadIO m) => Himari SimpleEnv a -> m a
+runSimpleEnv = runSimpleEnvWith defaultOutput stderr
+
+-- | `SimpleEnv`をカスタム出力で実行する。
+runSimpleEnvWith
+  :: (MonadIO m) => (Handle -> Loc -> LogSource -> LogLevel -> LogStr -> IO ()) -> Handle -> Himari SimpleEnv a -> m a
+runSimpleEnvWith logOutput toHandle action = do
+  let logAction' = \loc src level msg -> logOutput toHandle loc src level msg
+  runHimari (SimpleEnv logAction') action
--- src/Himari/Logger.hs
+++ src/Himari/Logger.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE FunctionalDependencies #-}
 
 module Himari.Logger
-  ( HasLogger (..)
+  ( HasLogAction (..)
+  , LogAction
   ) where
 
 import Himari.Prelude
 
-class HasLogger s a | s -> a where
-  logger :: Lens' s a
+class HasLogAction s a | s -> a where
+  logAction :: Lens' s a
+
+-- | ログ出力を行う関数の型。
+-- 出力をカスタムしたい場合このシグネチャに合わせた関数を作成するとやりやすい。
+type LogAction = Loc -> LogSource -> LogLevel -> LogStr -> IO ()
