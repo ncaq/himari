36cfb851cb0e3f4160df40fa4e4bb3f6c83c391d
ncaq <ncaq@ncaq.net>
2025-12-29 19:24:26 +0900

    refactor(test): itWithOuterとbeforeAllでテストの重複を削減
    - itWithOuterを使い各テストでrunHlintOnSampleの呼び出しを共通化
    - beforeAllでrunHlintOnSampleを一度だけ実行しテスト効率を向上
    - 各テストケースの重複コードを削除し可読性を改善

--- test/HlintUnliftioSpec.hs
+++ test/HlintUnliftioSpec.hs
@@ -10,53 +10,41 @@ import Test.Syd
 
 spec :: Spec
 spec = do
-  describe "hlint unliftio preference rules" $ do
-    it "Control.Concurrent.Async.async should warn to use UnliftIO.Async" $ do
-      output <- runHlintOnSample
+  describe "hlint unliftio preference rules" . beforeAll runHlintOnSample $ do
+    itWithOuter "Control.Concurrent.Async.async should warn to use UnliftIO.Async" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Async.async"
 
-    it "Control.Concurrent.Async.withAsync should warn to use UnliftIO.Async" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.Async.withAsync should warn to use UnliftIO.Async" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Async.withAsync"
 
-    it "Control.Concurrent.Async.race should warn to use UnliftIO.Async" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.Async.race should warn to use UnliftIO.Async" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Async.race"
 
-    it "Control.Concurrent.Async.concurrently should warn to use UnliftIO.Async" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.Async.concurrently should warn to use UnliftIO.Async" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Async.concurrently"
 
-    it "Control.Concurrent.Async.mapConcurrently should warn to use UnliftIO.Async" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.Async.mapConcurrently should warn to use UnliftIO.Async" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Async.mapConcurrently"
 
-    it "Control.Exception.catch should warn to use UnliftIO.Exception" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Exception.catch should warn to use UnliftIO.Exception" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Exception.catch"
 
-    it "Control.Exception.bracket should warn to use UnliftIO.Exception" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Exception.bracket should warn to use UnliftIO.Exception" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Exception.bracket"
 
-    it "Control.Exception.finally should warn to use UnliftIO.Exception" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Exception.finally should warn to use UnliftIO.Exception" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Exception.finally"
 
-    it "Control.Exception.try should warn to use UnliftIO.Exception" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Exception.try should warn to use UnliftIO.Exception" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Exception.try"
 
-    it "Control.Concurrent.MVar.withMVar should warn to use UnliftIO.MVar" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.MVar.withMVar should warn to use UnliftIO.MVar" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.MVar.withMVar"
 
-    it "Control.Concurrent.MVar.modifyMVar_ should warn to use UnliftIO.MVar" $ do
-      output <- runHlintOnSample
+    itWithOuter "Control.Concurrent.MVar.modifyMVar_ should warn to use UnliftIO.MVar" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.MVar.modifyMVar_"
 
-    it "System.Timeout.timeout should warn to use UnliftIO.Timeout" $ do
-      output <- runHlintOnSample
+    itWithOuter "System.Timeout.timeout should warn to use UnliftIO.Timeout" $ \output -> do
       output `shouldSatisfy` containsWarning "UnliftIO.Timeout.timeout"
 
 runHlintOnSample :: IO Text
