e2b7ab3cf5bdc76e6ec2d9bb544d801136405e52
ncaq <ncaq@ncaq.net>
2025-12-29 14:31:10 +0900

    refactor(flake): mkForceをmkDefaultに変更し強い命令を減らす

--- flake.nix
+++ flake.nix
@@ -81,8 +81,8 @@
             # `ghc-version`だけではなく`variants`で定義したGHCバージョンも`nix flake check`で自動的にテストされます。
             # 個別ビルド: nix build .#ghc9122:himari:lib:himari
             flake.variants = {
-              ghc9102.compiler-nix-name = final.lib.mkForce "ghc9102"; # GHC 9.10.2
-              ghc9122.compiler-nix-name = final.lib.mkForce "ghc9122"; # GHC 9.12.2
+              ghc9102.compiler-nix-name = final.lib.mkDefault "ghc9102"; # GHC 9.10.2
+              ghc9122.compiler-nix-name = final.lib.mkDefault "ghc9122"; # GHC 9.12.2
             };
             shell = {
               tools = {
