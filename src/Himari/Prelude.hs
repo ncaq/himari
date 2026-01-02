0dac8b1a1453d9665b03ac215e36b10e200ec889
ncaq <ncaq@ncaq.net>
2026-01-02 09:43:08 +0900

    refactor(prelude): Aeson/Safeの再エクスポートを分離し整理
    シンプルであるべきHimari.Preludeモジュールのhaddockドキュメントが、
    膨大になりすぎてしまっていたため。
    
    - Himari.Prelude.Aeson, Himari.Prelude.Safeモジュールを新規追加し、
      Aeson系・Safe系のre-exportを分離
    - Himari.PreludeからAeson/Safe関連のimport/hiding記述を削除し、
      新モジュール経由で再エクスポートする形に整理
    - himari.cabalに新モジュールをエクスポート対象として追加
    - コードの可読性と保守性を向上

--- src/Himari/Prelude.hs
+++ src/Himari/Prelude.hs
@@ -57,122 +57,14 @@ import Data.Void as Export
 import Data.Word as Export
 import Debug.Pretty.Simple as Export
 import GHC.Generics as Export hiding (from, to)
+import Himari.Prelude.Aeson as Export
+import Himari.Prelude.Safe as Export
 import System.FilePath as Export hiding ((<.>))
 import System.Process.Typed as Export
 import Text.Pretty.Simple as Export
 import Text.Show as Export
 import UnliftIO as Export
 import Prelude as Export
-
--- 部分関数ではない代替関数を提供してくれるsafeライブラリからシンボルを持ってきます。
--- 必要なのは部分関数ではない関数なので、部分関数はいらないのでhidingします。
--- `Safe.Foldable`の方がより汎用的なので、そちらを優先してコンフリクトするものはhidingします。
-import Safe as Export hiding
-  ( abort
-  , assertNote
-  , at
-  , atNote
-  , cycleNote
-  , elemIndexJust
-  , elemIndexJustNote
-  , findIndexJust
-  , findIndexJustNote
-  , findJust
-  , findJustDef
-  , findJustNote
-  , foldl1Def
-  , foldl1May
-  , foldl1Note
-  , foldl1Note'
-  , foldr1Def
-  , foldr1May
-  , foldr1Note
-  , fromJustNote
-  , headErr
-  , headNote
-  , indexNote
-  , initNote
-  , lastNote
-  , lookupJust
-  , lookupJustNote
-  , maximumBound
-  , maximumBoundBy
-  , maximumBounded
-  , maximumByDef
-  , maximumByMay
-  , maximumByNote
-  , maximumDef
-  , maximumMay
-  , maximumNote
-  , minimumBound
-  , minimumBoundBy
-  , minimumBounded
-  , minimumByDef
-  , minimumByMay
-  , minimumByNote
-  , minimumDef
-  , minimumMay
-  , minimumNote
-  , predNote
-  , readNote
-  , scanl1Note
-  , scanr1Note
-  , succNote
-  , tailErr
-  , tailNote
-  , toEnumNote
-  )
-import Safe.Exact as Export hiding
-  ( dropExact
-  , dropExactNote
-  , splitAtExact
-  , splitAtExactNote
-  , takeExact
-  , takeExactNote
-  , zip3Exact
-  , zip3ExactNote
-  , zipExact
-  , zipExactNote
-  , zipWith3Exact
-  , zipWith3ExactNote
-  , zipWithExact
-  , zipWithExactNote
-  )
-import Safe.Foldable as Export hiding
-  ( findJust
-  , findJustNote
-  , foldl1Note
-  , foldr1Note
-  , maximumByNote
-  , maximumNote
-  , minimumByNote
-  , minimumNote
-  )
-
--- aeson系には被りそうな関数名が多いので、hidingしたり、シンボルを個別importします。
-import Data.Aeson as Export hiding
-  ( decode
-  , decode'
-  , decodeFileStrict
-  , decodeFileStrict'
-  , decodeStrict
-  , decodeStrict'
-  , decodeStrictText
-  , eitherDecode'
-  , eitherDecodeFileStrict'
-  , eitherDecodeStrict'
-  , throwDecode'
-  , throwDecodeStrict'
-  , (.=)
-  )
-import Data.Aeson.Encode.Pretty as Export
-  ( encodePretty
-  , encodePretty'
-  , encodePrettyToTextBuilder
-  , encodePrettyToTextBuilder'
-  )
-import Data.Aeson.QQ.Simple as Export
-import Deriving.Aeson as Export
 
 -- Type only.
 
---
+++ src/Himari/Prelude/Safe.hs
@@ -0,0 +1,88 @@
+-- | Safe alternatives to partial functions from the safe library.
+-- Only total functions are exported; partial functions are hidden.
+-- Safe.Foldable is preferred for more generic versions, so conflicting functions are hidden from Safe.
+module Himari.Prelude.Safe
+  ( module Export
+  ) where
+
+import Safe as Export hiding
+  ( abort
+  , assertNote
+  , at
+  , atNote
+  , cycleNote
+  , elemIndexJust
+  , elemIndexJustNote
+  , findIndexJust
+  , findIndexJustNote
+  , findJust
+  , findJustDef
+  , findJustNote
+  , foldl1Def
+  , foldl1May
+  , foldl1Note
+  , foldl1Note'
+  , foldr1Def
+  , foldr1May
+  , foldr1Note
+  , fromJustNote
+  , headErr
+  , headNote
+  , indexNote
+  , initNote
+  , lastNote
+  , lookupJust
+  , lookupJustNote
+  , maximumBound
+  , maximumBoundBy
+  , maximumBounded
+  , maximumByDef
+  , maximumByMay
+  , maximumByNote
+  , maximumDef
+  , maximumMay
+  , maximumNote
+  , minimumBound
+  , minimumBoundBy
+  , minimumBounded
+  , minimumByDef
+  , minimumByMay
+  , minimumByNote
+  , minimumDef
+  , minimumMay
+  , minimumNote
+  , predNote
+  , readNote
+  , scanl1Note
+  , scanr1Note
+  , succNote
+  , tailErr
+  , tailNote
+  , toEnumNote
+  )
+import Safe.Exact as Export hiding
+  ( dropExact
+  , dropExactNote
+  , splitAtExact
+  , splitAtExactNote
+  , takeExact
+  , takeExactNote
+  , zip3Exact
+  , zip3ExactNote
+  , zipExact
+  , zipExactNote
+  , zipWith3Exact
+  , zipWith3ExactNote
+  , zipWithExact
+  , zipWithExactNote
+  )
+import Safe.Foldable as Export hiding
+  ( findJust
+  , findJustNote
+  , foldl1Note
+  , foldr1Note
+  , maximumByNote
+  , maximumNote
+  , minimumByNote
+  , minimumNote
+  )
